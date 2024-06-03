110d7f4e5da3d60bc56e63025a5bc51e0d05e154
nvermill12 <141042989+nvermill12@users.noreply.github.com>
2024-06-03 16:01:19 -0500

    Part 1

---
+++ Spirit Hunt/Assets/Scripts/MouseMovement.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseMovement : MonoBehaviour
+{
+    public float mouseSensitivity = 100f;
+
+    float xRotation = 0f;
+    float yRotation = 0f;
+    
+    void Start()
+    {
+
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    
+    void Update()
+    {
+        float mouseX = Input.Get
+        
+    }
+}
