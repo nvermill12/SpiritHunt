7f5ef98c620253042242048dd46fe341c0f98120
nvermill12 <141042989+nvermill12@users.noreply.github.com>
2024-06-04 15:52:44 -0500

    Part 2

--- Spirit Hunt/Assets/Scripts/MouseMovement.cs
+++ Spirit Hunt/Assets/Scripts/MouseMovement.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class MouseMovement : MonoBehaviour
 {
-    public float mouseSensitivity = 100f;
+    public float mouseSensitivity = 500f;
 
     float xRotation = 0f;
     float yRotation = 0f;
+
+    public float topClamp = -90f;
+    public float bottomClamp = 90f;
+
     
     void Start()
     {
@@ -18,7 +22,16 @@ public class MouseMovement : MonoBehaviour
     
     void Update()
     {
-        float mouseX = Input.Get
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
         
+        xRotation -= mouseY;
+
+        xRotation = Mathf.Clamp(xRotation, topClamp, bottomClamp);
+
+        yRotation += mouseX;
+
+        transform.localRotation = Quaternion.Euler(xRotation, yRotation, 0f);
+
     }
 }
--- Spirit Hunt/Assets/Scripts/PlayerMovement.cs
+++ Spirit Hunt/Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private CharacterController controller;
+
+    public float speed = 12f;
+    public float gravity = -9.8f * 2;
+    public float jumpHeight = 3f; 
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask; 
+
+    Vector3 velocity;
+
+    bool isGrounded;
+    bool isMoving;
+
+    private Vector3 lastPosition = new Vector3(0f,0f,0f);
     void Start()
     {
-        
+        controller = GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
-        
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance,groundMask);
+
+        if(isGrounded && velocity.y <0)
+        {
+            
+        }
     }
 }
