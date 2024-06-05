3142dae54bc6cd059ca71f8946d1b896deb02bad
nvermill12 <141042989+nvermill12@users.noreply.github.com>
2024-06-05 15:45:25 -0500

    Part 2

--- Spirit Hunt/Assets/Scripts/PlayerMovement.cs
+++ Spirit Hunt/Assets/Scripts/PlayerMovement.cs
@@ -19,20 +19,50 @@ public class PlayerMovement : MonoBehaviour
     bool isGrounded;
     bool isMoving;
 
-    private Vector3 lastPosition = new Vector3(0f,0f,0f);
+    private Vector3 lastPosition = new Vector3(0f, 0f, 0f);
+
     void Start()
     {
         controller = GetComponent<CharacterController>();
     }
 
-    
     void Update()
     {
-        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance,groundMask);
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
 
-        if(isGrounded && velocity.y <0)
+        if (isGrounded && velocity.y < 0)
         {
-            
+            velocity.y = -2f;
         }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed * Time.deltaTime);
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+
+        // Check if the player has moved and is grounded
+        if (lastPosition != transform.position && isGrounded)
+        {
+            isMoving = true;
+        }
+        else
+        {
+            isMoving = false; 
+        }
+
+        // Update lastPosition to the current position
+        lastPosition = transform.position;
     }
 }
+
